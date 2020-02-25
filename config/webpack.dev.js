62892731a51baa4ba4754d723d77e7e1d9416ea2
anjasdwi <anjasdwi@bareksa.com>
2020-02-25 16:58:21 +0700

    setup watch nodemon

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const webpack = require("webpack")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
 
 
 module.exports = {
@@ -46,15 +47,6 @@ module.exports = {
 				test:/\.html$/,
 				use: [
 					{
-						loader: "file-loader",
-						options: {
-							name: "[name].html",
-						},
-					},
-					{
-						loader: "extract-loader"
-					},
-					{
 						loader: "html-loader",
 						options: {
 							attrs: ["img:src"]
@@ -77,6 +69,9 @@ module.exports = {
 		]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    })
   ]
 }
